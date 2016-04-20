c5770c35447297e50527571546b160a47dfcf826
laurefr <laureguery@hotmail.com>
2016-04-20 21:59:09 +0100

    Array

---
+++ Array.js
@@ -0,0 +1,17 @@
+//// create an event listener
+//var Event1 = document.getElementById("button1");
+//event1 = addEventListener("click", myInterest, false);
+//
+//function myInterest(){
+    
+//    var userinput = document.getElementById("box").value;
+//    var userinput2 = document.getElementById("box2").value;
+//    var userinput3 = document.getElementById("box3").value;
+    
+
+    fruit1 =["kiwi" ,  "lemon" , "tomato" ];
+    fruit2= ["melon" ,  "fraise" ,  "pomme"];
+    fruit3 = ["cerise" ,  "poire" ,  "raisin"];
+    
+    document.getElementById("output").innerHTML=fruit1 + "<br>" + fruit2 + "<br>" + fruit3;
+

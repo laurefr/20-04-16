2a9afec492e9a525875fdc49a9383ca104055666
Laure Guery <laureguery@hotmail.com>
2016-04-21 09:30:53 +0100

    add

--- interestCal.js
+++ interestCal.js
@@ -1,18 +1,20 @@
 // create an event listener
-var Event1 = document.getElementById("button1");
-event1 = addEventListener("click", myInterest, false);
+//var event1 = document.getElementById("button1");
+//event1.addEventListener("click", myInterest, false);
+
+var event1 = document.getElementById("button1").addEventListener("click", myInterest, false);
+
 
 function myInterest(){
     
-    var userinput = document.getElementById("box").value;
-    var userinput2 = document.getElementById("box2").value;
-    var userinput3 = document.getElementById("box3").value;
+    var loan = parseInt(document.getElementById("loanbox").value);
+    var userinput2 = document.getElementById("ratebox").value;
+    var userinput3 = document.getElementById("termbox").value;
     
-    var loan = parseInt(userinput);
     var rate = parseInt(userinput2);
     var term = parseInt(userinput3);
-    //var text="";
     
+    //Calculations
    var interest = ((loan) * (rate/100) * (term));
     //text ="The total interest you will pay is" + "br" + "£";
     
@@ -20,6 +22,7 @@ function myInterest(){
     //text =" your monthly repayement will be" + "br" + "£";
     
 
+    //Output
     document.getElementById("output").innerHTML ="The total interest you will pay is"  + "£" + interest;
    
     document.getElementById("output2").innerHTML = "The total interest you will pay is"  + "£" + monthlyRepayement;
